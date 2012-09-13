3c83a94209e5e824caaabdcf25b0852f8ff943e2
Stuart Whelan <stuart@somepointinthefuture.co.nz>
2012-09-13 15:12:54 +1200

    Many fixes and begun work on using global config variables.

--- src/main/java/org/jvnet/hudson/plugins/VaultSCM.java
+++ src/main/java/org/jvnet/hudson/plugins/VaultSCM.java
@@ -5,43 +5,40 @@
  */
 package org.jvnet.hudson.plugins;
 
-import org.jvnet.hudson.plugins.VaultSCMRevisionState;
-import org.jvnet.hudson.plugins.VaultSCMChangeLogParser;
-import org.jvnet.hudson.plugins.VaultSCMChangeLogSet;
-import java.io.*;
-import java.text.SimpleDateFormat;
-import java.util.*;
-
-import net.sf.json.JSONObject;
-
-import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.StaplerRequest;
-
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
-import hudson.model.Run;
 import hudson.model.BuildListener;
+import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.scm.ChangeLogParser;
 import hudson.scm.PollingResult;
-import hudson.scm.SCMDescriptor;
 import hudson.scm.SCM;
+import hudson.scm.SCMDescriptor;
 import hudson.scm.SCMRevisionState;
 import hudson.util.ArgumentListBuilder;
-
-//XML parsing
+import hudson.util.FormValidation;
+import hudson.util.Secret;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import sun.nio.cs.ext.ISCII91;
 
 public class VaultSCM extends SCM {
+
+    private static final Logger LOG = Logger.getLogger(VaultSCM.class.getName());
 
     public static class VaultSCMDescriptor extends SCMDescriptor<VaultSCM> {
 
@@ -58,7 +55,7 @@ public class VaultSCM extends SCM {
          */
         @Override
         public String getDisplayName() {
-            return "Vault SCM";
+            return "SourceGear Vault";
         }
 
         @Override
@@ -72,38 +69,51 @@ public class VaultSCM extends SCM {
         public List<String> getMergeOptions() {
             return MERGE_OPTIONS;
         }
+
+        public FormValidation doCheckServerName(@QueryParameter String value) throws IOException, ServletException {
+            return FormValidation.validateRequired(value);
+        }
+
+        public FormValidation doCheckVaultExecutable(@QueryParameter String value) throws IOException, ServletException {
+            return FormValidation.validateRequired(value);
+        }
+
+        public FormValidation doCheckRepositoryName(@QueryParameter String value) throws IOException, ServletException {
+            return FormValidation.validateRequired(value);
+        }
+
+        public FormValidation doCheckPath(@QueryParameter String value) throws IOException, ServletException {
+            return FormValidation.validateRequired(value);
+        }
     }
     //configuration variables from user interface
-    private String server;
+    private String serverName;
     private String userName;
-    private String password;
-    private String repository; //name of the repository
-    private String vaultSCMExecutable;
+    private Secret password;
+    private String repositoryName; //name of the repository
+    private String vaultExecutable;
     private String path; //path in repository. Starts with $ sign.
-    private Boolean ssl; //ssl enabled?
-    private String merge = "overwrite"; //merge options: automatic, overwrite, later
-    private Boolean makeWriteable;
+    private Boolean sslEnabled; //ssl enabled?
+    private String merge;
+    private Boolean makeWritableEnabled;
 
-    public Boolean getMakeWriteable() {
-        return makeWriteable;
+    public Boolean getMakeWritableEnabled() {
+        return makeWritableEnabled;
     }
 
-    public void setMakeWriteable(Boolean makeWriteable) {
-        this.makeWriteable = makeWriteable;
+    public void setMakeWritableEnabled(Boolean makeWritableEnabled) {
+        this.makeWritableEnabled = makeWritableEnabled;
     }
 
-    public Boolean getSsl() {
-        return ssl;
+    public Boolean getSslEnabled() {
+        return sslEnabled;
     }
 
-    public void setSsl(Boolean ssl) {
-        this.ssl = ssl;
+    public void setSslEnabled(Boolean sslEnabled) {
+        this.sslEnabled = sslEnabled;
     }
 
     public String getMerge() {
-        if (merge == null || merge.isEmpty()) {
-            this.merge = "overwrite";
-        }
         return merge;
     }
 
@@ -120,12 +130,12 @@ public class VaultSCM extends SCM {
     }
     //getters and setters
 
-    public String getServer() {
-        return server;
+    public String getServerName() {
+        return serverName;
     }
 
-    public void setServer(String server) {
-        this.server = server;
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
     }
 
     public String getUserName() {
@@ -137,27 +147,27 @@ public class VaultSCM extends SCM {
     }
 
     public String getPassword() {
-        return password;
+        return Secret.toString(password);
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        this.password = Secret.fromString(password);
     }
 
-    public String getRepository() {
-        return repository;
+    public String getRepositoryName() {
+        return repositoryName;
     }
 
-    public void setRepository(String repository) {
-        this.repository = repository;
+    public void setRepositoryName(String repositoryName) {
+        this.repositoryName = repositoryName;
     }
 
-    public String getVaultSCMExecutable() {
-        return this.vaultSCMExecutable;
+    public String getVaultExecutable() {
+        return this.vaultExecutable;
     }
 
-    public void setVaultSCMExecutable(String vaultSCMExecutable) {
-        this.vaultSCMExecutable = vaultSCMExecutable;
+    public void setVaultSCMExecutable(String vaultExecutable) {
+        this.vaultExecutable = vaultExecutable;
     }
     /**
      * Singleton descriptor.
@@ -168,17 +178,18 @@ public class VaultSCM extends SCM {
     public static final SimpleDateFormat VAULT_DATETIME_FORMATTER = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 
     @DataBoundConstructor
-    public VaultSCM(String server, String path, String userName,
-            String password, String repository, String vaultSCMExecutable, Boolean ssl, String merge, Boolean makeWriteable) {
-        this.server = server;
+    public VaultSCM(String serverName, String path, String userName,
+            String password, String repositoryName, String vaultExecutable, Boolean sslEnabled, String merge, Boolean makeWritableEnabled) {
+        this.serverName = serverName;
         this.userName = userName;
-        this.password = password;
-        this.repository = repository;
-        this.vaultSCMExecutable = vaultSCMExecutable;
+        this.password = Secret.fromString(password);
+        this.repositoryName = repositoryName;
+        this.vaultExecutable = vaultExecutable;
         this.path = path;
-        this.ssl = ssl; //Default to true
+        this.sslEnabled = sslEnabled; //Default to true
         this.merge = merge.isEmpty() ? "overwrite" : merge;
-        this.makeWriteable = makeWriteable;
+        this.makeWritableEnabled = makeWritableEnabled;
+
     }
 
     @Override
@@ -207,6 +218,7 @@ public class VaultSCM extends SCM {
             throws IOException, InterruptedException {
 
         Date lastBuild = ((VaultSCMRevisionState) baseline).getDate();
+        LOG.log(Level.INFO, "Last Build Date set to {0}", lastBuild.toString());
         Date now = new Date();
         File temporaryFile = File.createTempFile("changes", "txt");
         int countChanges = determineChangeCount(launcher, workspace, listener, lastBuild, now, temporaryFile);
@@ -216,8 +228,6 @@ public class VaultSCM extends SCM {
         } else {
             return PollingResult.BUILD_NOW;
         }
-
-
     }
 
     @Override
@@ -226,41 +236,38 @@ public class VaultSCM extends SCM {
             throws IOException, InterruptedException {
         boolean returnValue = true;
 
-        if (server != null) {
-            listener.getLogger().println("server: " + server);
+        if (serverName != null) {
+            listener.getLogger().println("server: " + serverName);
         }
 
         //populate the GET command
         //in some cases username, host and password can be empty e.g. if rememberlogin is used to store login data
         ArgumentListBuilder argBuildr = new ArgumentListBuilder();
-        argBuildr.add(getVaultSCMExecutable());
+        argBuildr.add(getVaultExecutable());
         argBuildr.add("GET");
 
-        if (!server.isEmpty()) {
-            argBuildr.add("-host", server);
+        if (!serverName.isEmpty()) {
+            argBuildr.add("-host", serverName);
         }
 
         if (!userName.isEmpty()) {
             argBuildr.add("-user", userName);
         }
 
-        if (!password.isEmpty()) {
-            argBuildr.add("-password", password);
+        if (!Secret.toString(password).isEmpty()) {
+            argBuildr.add("-password");
+            argBuildr.add(Secret.toString(password), true);
         }
 
-        if (!repository.isEmpty()) {
-            argBuildr.add("-repository", repository);
+        if (!repositoryName.isEmpty()) {
+            argBuildr.add("-repository", repositoryName);
         }
 
-        if (this.ssl) {
+        if (this.sslEnabled) {
             argBuildr.add("-ssl");
         }
 
-        if (this.ssl) {
-            argBuildr.add("-ssl");
-        }
-
-        if (this.makeWriteable) {
+        if (this.makeWritableEnabled) {
             argBuildr.add("-makewritable");
         }
 
@@ -269,8 +276,7 @@ public class VaultSCM extends SCM {
         argBuildr.add("-workingfolder", workspace.getRemote());
         argBuildr.add(this.path);
 
-        int cmdResult = launcher.launch().cmds(argBuildr).envs(build.getEnvironment(TaskListener.NULL))
-                .stdout(listener.getLogger()).pwd(workspace).join();
+        int cmdResult = launcher.launch().cmds(argBuildr).envs(build.getEnvironment(TaskListener.NULL)).stdout(listener.getLogger()).pwd(workspace).join();
         if (cmdResult == 0) {
             final Run<?, ?> lastBuild = build.getPreviousBuild();
             final Date lastBuildDate;
@@ -315,26 +321,27 @@ public class VaultSCM extends SCM {
             try {
 
                 ArgumentListBuilder argBuildr = new ArgumentListBuilder();
-                argBuildr.add(getVaultSCMExecutable());
+                argBuildr.add(getVaultExecutable());
                 argBuildr.add("VERSIONHISTORY");
 
-                if (!server.isEmpty()) {
-                    argBuildr.add("-host", server);
+                if (!serverName.isEmpty()) {
+                    argBuildr.add("-host", serverName);
                 }
 
                 if (!userName.isEmpty()) {
                     argBuildr.add("-user", userName);
                 }
 
-                if (!password.isEmpty()) {
-                    argBuildr.add("-password", password);
+                if (!Secret.toString(password).isEmpty()) {
+                    argBuildr.add("-password");
+                    argBuildr.add(Secret.toString(password), true);
                 }
 
-                if (!repository.isEmpty()) {
-                    argBuildr.add("-repository", repository);
+                if (!repositoryName.isEmpty()) {
+                    argBuildr.add("-repository", repositoryName);
                 }
 
-                if (this.ssl) {
+                if (this.sslEnabled) {
                     argBuildr.add("-ssl");
                 }
 
@@ -379,26 +386,27 @@ public class VaultSCM extends SCM {
             try {
 
                 ArgumentListBuilder argBuildr = new ArgumentListBuilder();
-                argBuildr.add(getVaultSCMExecutable());
+                argBuildr.add(getVaultExecutable());
                 argBuildr.add("VERSIONHISTORY");
 
-                if (!server.isEmpty()) {
-                    argBuildr.add("-host", server);
+                if (!serverName.isEmpty()) {
+                    argBuildr.add("-host", serverName);
                 }
 
                 if (!userName.isEmpty()) {
                     argBuildr.add("-user", userName);
                 }
 
-                if (!password.isEmpty()) {
-                    argBuildr.add("-password", password);
+                if (!Secret.toString(password).isEmpty()) {
+                    argBuildr.add("-password");
+                    argBuildr.add(Secret.toString(password), true);
                 }
 
-                if (!repository.isEmpty()) {
-                    argBuildr.add("-repository", repository);
+                if (!repositoryName.isEmpty()) {
+                    argBuildr.add("-repository", repositoryName);
                 }
 
-                if (this.ssl) {
+                if (this.sslEnabled) {
                     argBuildr.add("-ssl");
                 }
 
@@ -433,12 +441,9 @@ public class VaultSCM extends SCM {
 
         return result;
     }
+    
     private final static String[] MERGE_OPTION_STRINGS = new String[]{
         "automatic",
         "overwrite",
         "later",};
-//    private final static List<String> MERGE_OPTIONS = new ArrayList<String>();
-//    static {
-//        MERGE_OPTIONS.addAll(Arrays.asList(MERGE_OPTION_STRINGS));
-//    }
 }
